b509d804315598a04470a121a8d14857e3d3a4f5
Nick-M-R <149333463+Nick-M-R@users.noreply.github.com>
2024-10-18 16:36:22 -0700

    Add files via upload
    I changed some stuff for you guys. I set up the first array

--- November 1st NewbieHW 1.py
+++ November 1st NewbieHW 1.py
@@ -1,2 +1,18 @@
 # November 1st NewbieHW 1
-print("testing the git hub")
+# Team newbie
+
+# imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Problem 2: Grid Generation & Polar coordinates
+
+# Unit variable setups
+R_cyl = 1.0
+U_inf = 1
+
+# Grid variable setup
+rmin = R_cyl
+rmax = 3
+N_r = 11
+r = np.linspace(rmin,rmax,N_r)
